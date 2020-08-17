1b1349eb47e1a593fec333a2258e670ec28b5225
Olivier Goulet <oliviergoulet5@gmail.com>
2020-08-17 09:08:15 -0400

    Modularized middleware

---
+++ middleware.js
@@ -0,0 +1,40 @@
+import path from 'path';
+import fs from 'fs';
+import multer from 'multer';
+const { v4: uuidv4 } = require('uuid');
+
+export const attachId = (req, res, next) => {
+    req.id = uuidv4();
+    next();
+}
+
+// Multer
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        const dir = `./uploads/${req.id}`;
+        fs.exists(dir, exist => {
+            if (!exist) {
+                return fs.mkdir(dir, error => cb(error, dir));
+            }
+
+            return cb(null, dir);
+        })
+    },
+    filename: (req, file, cb) => {
+        let timeStamp = Date.now();
+        cb(null, file.fieldname + '-' + timeStamp + '.' + file.originalname.split('.')[file.originalname.split('.').length - 1])
+    }
+});
+
+export const upload = multer({ 
+    storage: storage,
+    fileFilter: (req, file, cb) => {
+        let extension = path.extname(file.originalname);
+
+        if (!acceptedFileFormats.test(extension)) {
+            return cb(new Error('File format is not allowed.'));
+        }
+
+        cb(null, true);
+    }
+}).single('attachments');
--- server.js
+++ server.js
@@ -1,56 +1,17 @@
 import express from 'express';
-const app = express();
-import multer from 'multer';
 import cors from 'cors';
-import path from 'path';
-import fs from 'fs';
-const { v4: uuidv4 } = require('uuid');
 
 import { storeData } from './util';
+import { attachId, upload } from './middleware';
 
 const HTTP_PORT = process.env.PORT || 8080;
+const app = express();
 
 // Stub Variables
 let acceptedFileFormats = /png|jpg/;
 
-// Multer Setup
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        const dir = `./uploads/${req.id}`;
-        fs.exists(dir, exist => {
-            if (!exist) {
-                return fs.mkdir(dir, error => cb(error, dir));
-            }
-
-            return cb(null, dir);
-        })
-    },
-    filename: (req, file, cb) => {
-        let timeStamp = Date.now();
-        cb(null, file.fieldname + '-' + timeStamp + '.' + file.originalname.split('.')[file.originalname.split('.').length - 1])
-    }
-});
-
-const upload = multer({ 
-    storage: storage,
-    fileFilter: (req, file, cb) => {
-        let extension = path.extname(file.originalname);
-
-        if (!acceptedFileFormats.test(extension)) {
-            return cb(new Error('File format is not allowed.'));
-        }
-
-        cb(null, true);
-    }
-}).single('attachments');
-
 // Middleware
 app.use(cors({ origin: 'http://localhost:3000' }));
-
-const attachId = (req, res, next) => {
-    req.id = uuidv4();
-    next();
-}
 
 // Routes
 app.post('/submit-feedback', attachId, upload, (req, res) => {
